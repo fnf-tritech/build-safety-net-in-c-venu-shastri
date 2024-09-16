0d32aa813f41353f91b1f3ab50b8ca7dd47eb893
Venugopal K <venu.shastri@gmail.com>
2024-09-16 10:44:53 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,4 +1,6 @@
 #include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
 void generateSoundexCode(const char* inputString, char* soudexCode){
    if(strlen(inputString) == 0) {
         strcpy(soudexCode,"\0");
